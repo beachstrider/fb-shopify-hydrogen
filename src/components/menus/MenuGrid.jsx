96cb76399fdefdeef74d989d3da481a179542721
whitebear0909 <boris_whitebear0909@outlook.com>
2022-12-12 17:27:39 -0500

    completed menu grid item

--- src/components/menus/MenuGrid.jsx
+++ src/components/menus/MenuGrid.jsx
@@ -1,14 +1,146 @@
 export function MenuGrid() {
     return (
         <div className="grid grid-cols-4 gap-4">
-            <div className="mb-3 items-stretch ">01</div>
-            <div className="mb-3 items-stretch ">02</div>
-            <div className="mb-3 items-stretch ">03</div>
-            <div className="mb-3 items-stretch ">04</div>
-            <div className="mb-3 items-stretch ">05</div>
-            <div className="mb-3 items-stretch ">06</div>
-            <div className="mb-3 items-stretch ">07</div>
-            <div className="mb-3 items-stretch ">08</div>
+            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" src="//cdn.shopify.com/s/files/1/0552/6549/3185/products/Balsamic_Maple_Chicken_LC_Web-127014_large.jpg?v=1670681924" alt="" />
+                </a>
+                <div class="p-2">
+                    <a href="#">
+                        <h5 class="mb-2 text-lg font-bold text-gray-900 dark:text-white">Balsamic Maple Chicken</h5>
+                    </a>
+                    <div className="grid grid-cols-4 gap-4">
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" src="//cdn.shopify.com/s/files/1/0552/6549/3185/products/Balsamic_Maple_Chicken_LC_Web-127014_large.jpg?v=1670681924" alt="" />
+                </a>
+                <div class="p-2">
+                    <a href="#">
+                        <h5 class="mb-2 text-lg font-bold text-gray-900 dark:text-white">Balsamic Maple Chicken</h5>
+                    </a>
+                    <div className="grid grid-cols-4 gap-4">
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" src="//cdn.shopify.com/s/files/1/0552/6549/3185/products/Balsamic_Maple_Chicken_LC_Web-127014_large.jpg?v=1670681924" alt="" />
+                </a>
+                <div class="p-2">
+                    <a href="#">
+                        <h5 class="mb-2 text-lg font-bold text-gray-900 dark:text-white">Balsamic Maple Chicken</h5>
+                    </a>
+                    <div className="grid grid-cols-4 gap-4">
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" src="//cdn.shopify.com/s/files/1/0552/6549/3185/products/Balsamic_Maple_Chicken_LC_Web-127014_large.jpg?v=1670681924" alt="" />
+                </a>
+                <div class="p-2">
+                    <a href="#">
+                        <h5 class="mb-2 text-lg font-bold text-gray-900 dark:text-white">Balsamic Maple Chicken</h5>
+                    </a>
+                    <div className="grid grid-cols-4 gap-4">
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img class="rounded-t-lg" src="//cdn.shopify.com/s/files/1/0552/6549/3185/products/Balsamic_Maple_Chicken_LC_Web-127014_large.jpg?v=1670681924" alt="" />
+                </a>
+                <div class="p-2">
+                    <a href="#">
+                        <h5 class="mb-2 text-lg font-bold text-gray-900 dark:text-white">Balsamic Maple Chicken</h5>
+                    </a>
+                    <div className="grid grid-cols-4 gap-4">
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                        <div className="px-2 text-sm font-bold">
+                            <div>44g</div>
+                            <div>Carbs</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
   }
