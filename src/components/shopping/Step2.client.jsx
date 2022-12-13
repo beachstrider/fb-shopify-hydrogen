ce1443cf84ae287897dfe5849b1ca2ce9a2371fb
Habib <habiburrahman0540@gmail.com>
2022-12-14 03:49:52 +0600

    CardQuantity component

--- src/components/shopping/Step2.client.jsx
+++ src/components/shopping/Step2.client.jsx
@@ -1,5 +1,26 @@
-import {Item} from '~/components';
-
+import CardQuantities from './components/CardQuantities'
+const CardData=[
+  {image:'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png',
+  title:'BBQ FEASTbox',
+  serves:5
+  },
+  {image:'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png',
+  title:'BBQ FEASTbox',
+  serves:5
+  },
+  {image:'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png',
+  title:'BBQ FEASTbox',
+  serves:5
+  },
+  {image:'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png',
+  title:'BBQ FEASTbox',
+  serves:5
+  },
+  {image:'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png',
+  title:'BBQ FEASTbox',
+  serves:5
+  }
+];
 export function Step2({...props}) {
   return (
     <div>
@@ -11,190 +32,30 @@ export function Step2({...props}) {
         2. Choose your Meals
       </label>
       <div className="flex flex-wrap -mx-2 -mb-2">
-        <Item />
-        <div className="w-1/3 lg:w-1/5 sm:w-1/3 md:w-1/3 p-2 text-center">
-          <div className="text-center">
-            <a className="block text-center font-bold font-heading" href="#">
-              <img
-                className="mx-auto object-contain"
-                src="https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png"
-                alt=""
+          {CardData.map((item,index)=>(
+              <CardQuantities 
+                    key={index}
+                    title={item.title}
+                    image={item.image}
+                    serves={item.serves}
               />
-              <h3 className="font-bold font-heading text-sm text-center">
-                BBQ FEASTbox
-              </h3>
-              <div className="text-center text-sm mb-2">Serves: 5</div>
-            </a>
-            <div className="px-4 mb-4 xl:mb-0 text-center">
-              <a
-                className="text-center text-white font-bold font-heading uppercase transition"
-                href="#"
-                style={{
-                  backgroundColor: '#DB9707',
-                  color: '#FFFFFF',
-                  width: '80px',
-                  padding: '3px 21px',
-                }}
-              >
-                Add+
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="w-1/3 lg:w-1/5 sm:w-1/3 md:w-1/3 p-2 text-center">
-          <div className="text-center">
-            <a className="block text-center font-bold font-heading" href="#">
-              <img
-                className="mx-auto object-contain"
-                src="https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png"
-                alt=""
-              />
-              <h3 className="font-bold font-heading text-sm text-center">
-                BBQ FEASTbox
-              </h3>
-              <div className="text-center text-sm mb-2">Serves: 5</div>
-            </a>
-            <div className="px-4 mb-4 xl:mb-0 text-center">
-              <a
-                className="text-center text-white font-bold font-heading uppercase transition"
-                href="#"
-                style={{
-                  backgroundColor: '#DB9707',
-                  color: '#FFFFFF',
-                  width: '80px',
-                  padding: '3px 21px',
-                }}
-              >
-                Add+
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="w-1/3 lg:w-1/5 sm:w-1/3 md:w-1/3 p-2 text-center">
-          <div className="text-center">
-            <a className="block text-center font-bold font-heading" href="#">
-              <img
-                className="mx-auto object-contain"
-                src="https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png"
-                alt=""
-              />
-              <h3 className="font-bold font-heading text-sm text-center">
-                BBQ FEASTbox
-              </h3>
-              <div className="text-center text-sm mb-2">Serves: 5</div>
-            </a>
-            <div className="px-4 mb-4 xl:mb-0 text-center">
-              <a
-                className="text-center text-white font-bold font-heading uppercase transition"
-                href="#"
-                style={{
-                  backgroundColor: '#DB9707',
-                  color: '#FFFFFF',
-                  width: '80px',
-                  padding: '3px 21px',
-                }}
-              >
-                Add+
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="w-1/3 lg:w-1/5 sm:w-1/3 md:w-1/3 p-2 text-center">
-          <div className="text-center">
-            <a className="block text-center font-bold font-heading" href="#">
-              <img
-                className="mx-auto object-contain"
-                src="https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png"
-                alt=""
-              />
-              <h3 className="font-bold font-heading text-sm text-center">
-                BBQ FEASTbox
-              </h3>
-              <div className="text-center text-sm mb-2">Serves: 5</div>
-            </a>
-            <div className="px-4 mb-4 xl:mb-0 text-center">
-              <a
-                className="text-center text-white font-bold font-heading uppercase transition"
-                href="#"
-                style={{
-                  backgroundColor: '#DB9707',
-                  color: '#FFFFFF',
-                  width: '80px',
-                  padding: '3px 21px',
-                }}
-              >
-                Add+
-              </a>
-            </div>
-          </div>
-        </div>
+          ))} 
       </div>
       <label
-        className="block text-gray-800 text-lg font-bold mb-2"
+        className="block text-gray-800 text-2xl font-bold mb-2 mt-6"
         htmlFor=""
-        style={{fontSize: '24px', marginTop: '20px'}}
       >
         Breakfast Meals
       </label>
       <div className="flex flex-wrap -mx-2 -mb-2">
-        <div className="w-1/3 lg:w-1/5 sm:w-1/3 md:w-1/3 p-2 text-center">
-          <div className="text-center">
-            <a className="block text-center font-bold font-heading" href="#">
-              <img
-                className="mx-auto object-contain"
-                src="https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png"
-                alt=""
-              />
-              <h3 className="font-bold font-heading text-sm text-center">
-                BBQ FEASTbox
-              </h3>
-              <div className="text-center text-sm mb-2">Serves: 5</div>
-            </a>
-            <div className="px-4 mb-4 xl:mb-0 text-center">
-              <a
-                className="text-center text-white font-bold font-heading uppercase transition"
-                href="#"
-                style={{
-                  backgroundColor: '#DB9707',
-                  color: '#FFFFFF',
-                  width: '80px',
-                  padding: '3px 21px',
-                }}
-              >
-                Add+
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="w-1/3 lg:w-1/5 sm:w-1/3 md:w-1/3 p-2 text-center">
-          <div className="text-center">
-            <a className="block text-center font-bold font-heading" href="#">
-              <img
-                className="mx-auto object-contain"
-                src="https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png"
-                alt=""
-              />
-              <h3 className="font-bold font-heading text-sm text-center">
-                BBQ FEASTbox
-              </h3>
-              <div className="text-center text-sm mb-2">Serves: 5</div>
-            </a>
-            <div className="px-4 mb-4 xl:mb-0 text-center">
-              <a
-                className="text-center text-white font-bold font-heading uppercase transition"
-                href="#"
-                style={{
-                  backgroundColor: '#DB9707',
-                  color: '#FFFFFF',
-                  width: '80px',
-                  padding: '3px 21px',
-                }}
-              >
-                Add+
-              </a>
-            </div>
-          </div>
-        </div>
+        {CardData.map((item,index)=>(
+                <CardQuantities 
+                      key={index}
+                      title={item.title}
+                      image={item.image}
+                      serves={item.serves}
+                />
+            ))} 
       </div>
     </div>
   );
