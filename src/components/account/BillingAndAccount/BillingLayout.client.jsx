0efbd24aaab598ec3c418099d574132a391b31e0
smartcleancode <smartdevcode@gmail.com>
2022-12-14 06:20:11 -0600

    Recharge subscription billing and account

---
+++ src/components/account/BillingAndAccount/BillingLayout.client.jsx
@@ -0,0 +1,54 @@
+import {Image, Link} from '@shopify/hydrogen';
+import axios from 'axios';
+import { useState } from 'react';
+
+// import Addresses from './Addresses.client';
+// import Billing from './Billing.client';
+// import Password from './Password.client';
+
+const BillingLayout = ({billinginfo}) => {
+  return (
+    <div className="px-12 py-4">
+      <div className="w-full max-w-2xl mb-4 text-3xl uppercase font-bold">
+        YOUR BILLING & ACCOUNT INFO
+      </div>
+      <div className="w-full max-w-2xl mb-4 text-lg">
+        You can update your card, billing address & account password below.
+      </div>
+
+      <h2 className='w-full text-2xl mb-4'>SHIPPING ADDRESS</h2>
+      <div className='mb-4'>
+        <p>{billinginfo.address.first_name} {billinginfo.address.last_name}</p>
+        <p>{billinginfo.address.address1}</p>
+        <p>{billinginfo.address.address2}</p>
+        <p>{billinginfo.address.city} {billinginfo.address.country_code} {billinginfo.address.zip}</p>
+        <Link
+          className={`block underline disabled:text-gray-400`}
+          to={`/account/shipping-address/${billinginfo.address.id}`}
+        >
+          Edit
+        </Link>
+      </div>
+
+      <h2 className='w-full text-2xl mb-4'>BILLING ADDRESS</h2>
+      <div className='mb-4'>
+        <p>{billinginfo.payment_method.billing_address.first_name} {billinginfo.payment_method.billing_address.last_name}</p>
+        <p>{billinginfo.payment_method.billing_address.address1}</p>
+        <p>{billinginfo.payment_method.billing_address.address2}</p>
+        <p>{billinginfo.payment_method.billing_address.city} {billinginfo.payment_method.billing_address.country_code} {billinginfo.payment_method.billing_address.zip}</p>
+      </div>
+
+      <h2 className='w-full text-2xl mb-4'>PAYMENT METHOD</h2>
+      <div className='mb-4'>
+        <p>{billinginfo.payment_method.payment_details.brand} ••••{billinginfo.payment_method.payment_details.last4}</p>
+        <p>Expires {billinginfo.payment_method.payment_details.exp_month}/{billinginfo.payment_method.payment_details.exp_year}</p>
+        <p>{billinginfo.payment_method.payment_details.brand}</p>
+      </div>
+      {/* <Addresses billinginfo={billinginfo} />
+      <Billing billinginfo={billinginfo} />
+      <Password billinginfo={billinginfo} /> */}
+    </div>
+  );
+};
+
+export default BillingLayout;
