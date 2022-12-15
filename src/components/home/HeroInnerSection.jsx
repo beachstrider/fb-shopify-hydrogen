8070d12aebb185cba71623be4fce1528ba798108
Habib <habiburrahman0540@gmail.com>
2022-12-15 20:02:50 +0600

    Home page update

--- src/components/home/HeroInnerSection.jsx
+++ src/components/home/HeroInnerSection.jsx
@@ -1,31 +1,27 @@
-import plateImage from "../../assets/plate.png"
+import Carousel1 from "../../assets/carousel_1.png"
+import Carousel2 from "../../assets/carousel_2.png"
+import Carousel3 from "../../assets/carousel_3.png"
 export function HeroInnerSection() {
     return (
-      <div className="h-[400px] bg-right bg-center relative overflow-hidden bg-no-repeat bg-cover pb-5 bg-image bg-[url('../assets/join_bg.png')]">
-        <div className="flex items-center justify-around h-full">
-            <div className="ml-[10.5rem]">
-                 <img
-                src={plateImage}
-                className="max-w-sm h-auto mt-[-130px] z-40"
-                alt=""
-              />
-              </div>
-              <div className="text-white text-lg">
-                <h5>3 Family Meals a Week</h5>
-                <h2>MouthWatering Grub For $8.5 person</h2>
-                <p>Here's to epic flavors with ready -to-eat convenence at a fraction of the cost of restaurent delivery.Now that was a mouthful ! just like our food. </p>
-                <div className="flex item-center mt-5">
-                <button className="bg-[#A60D1E] text-white font-bold py-2 px-4 w-[40px]">
-                  Get Eating
-                </button>
-                  <div className="ml-[20px]">
-                    <h2>Subscribe now for free</h2>
-                    <h2>Breakfast!Cancel anytime!</h2>
-                  </div>
-                </div>
+<section className="h-[420px]  bg-right bg-center relative overflow-hidden bg-no-repeat bg-cover pb-5 bg-image bg-[url('../assets/join_bg.png')]">
+    <div className="container max-w-screen-xl m-auto">
+       <h1 className="text-center text-[38px] text-white uppercase font-extrabold leading-[80px]">Welcome to the robot-free zone</h1>
+       <p className="text-center text-[20px] text-white capitalize">Just clean, Natural ingredients Prepared by Clean, Natural Humans</p>
+       <div className="flex items-center justify-center pt-[15px]">
+            <div className="w-1/3 ml-[50px]">
+                  <img src={Carousel1} className="w-[350px] h-[210px]" alt="" />
             </div>
-            
+            <div className="w-1/3 ml-[50px]">
+                  <img src={Carousel2} className="w-[350px] h-[210px]" alt="" />
+            </div>
+            <div className="w-1/3 ml-[50px]">
+                  <img src={Carousel3} className="w-[350px] h-[210px]" alt="" />
+            </div>
+       </div>
+        <div className="text-center py-5">
+          <button className="bg-[#A60D1E] text-white font-bold px-8 py-2">Learn More</button>
         </div>
-      </div>
-    );
+    </div>
+</section>
+       );
   }
