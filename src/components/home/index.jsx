393bf73162723e6caf3f84a17c9a629f617afc2b
Habib <habiburrahman0540@gmail.com>
2022-12-16 20:44:43 +0600

    Slider added to Home page

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,4 +1,4 @@
-export {HeroInnerSection} from './HeroInnerSection';
+export {HeroInnerSection} from './HeroInnerSection.client';
 export {HeroSection} from './HeroSection';
 export {PartyTime} from './PartyTime';
 export {CounterSection} from './CounterSection';
