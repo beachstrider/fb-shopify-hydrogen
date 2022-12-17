ff5747c95a4589b2c409b9b710cd878acc1e32af
Jason Heo <mernstackdev729@gmail.com>
2022-12-17 22:13:01 +0900

    testing

--- src/routes/api/account/orders/__.server.js
+++ src/routes/api/account/orders/__.server.js
@@ -4,10 +4,10 @@ import {headers} from '~/lib/recharge';
 export async function api(request, {session}) {
   const external_customer_id = 6711027073315;
 
-  const orders = fetchSync(
+  const res = fetchSync(
     `https://api.rechargeapps.com/orders?customer_id=100937547&status=success&sort_by=scheduled_at-asc`,
     {headers},
   );
 
-  return orders;
+  return res.json();
 }
