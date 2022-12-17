6accab0ed8e12e78e20064acd441ea0cad4928f0
Jason Heo <mernstackdev729@gmail.com>
2022-12-17 20:42:39 +0900

    testing

---
+++ src/routes/api/account/orders/_.server.js
@@ -0,0 +1,20 @@
+import {getUpcomingOrdersAxios} from '~/lib/recharge';
+
+export async function api(request, {session}) {
+  const external_customer_id = 6711027073315;
+
+  // if (session) {
+  //   const {customerAccessToken} = await session.get();
+
+  //   if (!customerAccessToken)
+  //     return new Response(
+  //       JSON.stringify({message: 'No session'}, {status: 400}),
+  //     );
+
+  //   const orders = await getUpcomingOrdersAxios({external_customer_id});
+
+  //   return orders;
+  // }
+
+  return new Response('Error');
+}
