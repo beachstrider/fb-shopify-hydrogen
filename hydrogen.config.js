49461cc501cc7a9367f22cd0f8b23543929ec585
Jason Heo <mernstackdev729@gmail.com>
2022-12-19 09:45:42 +0900

    test

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     privateStorefrontToken: Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN, // eslint-disable-line no-undef
     storefrontApiVersion: '2022-07',
     storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID, // eslint-disable-line no-undef
+    rechargeToken: Oxygen?.env?.PUBLIC_RECHARGE_API_TOKEN, // eslint-disable-line no-undef
   },
   session: CookieSessionStorage('__session', {
     path: '/',
