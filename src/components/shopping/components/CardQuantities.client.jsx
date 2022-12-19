669db77463cc5f1b78f98dc852662c19c9e337a6
Habib <habiburrahman0540@gmail.com>
2022-12-20 01:07:56 +0600

    font color change

--- src/components/shopping/components/CardQuantities.client.jsx
+++ src/components/shopping/components/CardQuantities.client.jsx
@@ -10,10 +10,10 @@ const CardQuantities = ({title, image, serves}) => {
             <div onClick={() => setOpenModal(true)}>
               <img className="mx-auto object-contain" src={image} alt="" />
             </div>
-            <h3 className="font-bold font-heading text-sm text-center">
+            <h3 className="font-bold font-heading text-sm text-center text-[#2C2C2C]">
               {title}
             </h3>
-            <div className="text-center text-sm mb-2">Serves: {serves}</div>
+            <div className="text-center text-sm mb-2 text-[#2C2C2C]">Serves: {serves}</div>
           </a>
           <div className="px-4 mb-4 xl:mb-0 text-center">
             <a
@@ -36,32 +36,32 @@ const CardQuantities = ({title, image, serves}) => {
          {/*header*/}
          <button
              type="button"
-              class="absolute -top-1 -right-1 rounded-full border border-gray-200 bg-white p-1 text-gray-800"
+             className="absolute -top-1 -right-1 rounded-full border border-gray-200 bg-white p-1 text-gray-800"
               onClick={() => setOpenModal(false)}
            >
-            <span class="sr-only">Close</span>
+            <span className="sr-only">Close</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-4 w-4"
+              className="h-4 w-4"
               viewBox="0 0 20 20"
               fill="black 800"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
          </button>
-         <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 px-5 py-5" >
+         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 px-5 py-5" >
       <img
         alt="Laptop"
         src="https://images.unsplash.com/photo-1587614382346-4ec70e388b28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-        class="h-full w-full object-cover"
+        className="h-full w-full object-cover"
       />
 
       <div>
-        <h1 class="text-lg font-bold text-center">
+        <h1 className="text-lg font-bold text-center">
           BBQ FeastBox
         </h1>
       </div>
@@ -95,7 +95,7 @@ const CardQuantities = ({title, image, serves}) => {
    </div>
    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
  </>
-      ):null}
+      ):" "}
     </>
   );
 };
