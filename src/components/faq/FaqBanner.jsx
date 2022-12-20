6e95a9274bd404a40035d3667e76db79843ea18d
whitebear0909 <boris_whitebear0909@outlook.com>
2022-12-20 13:17:29 -0500

    fixed faq page banner text

--- src/components/faq/FaqBanner.jsx
+++ src/components/faq/FaqBanner.jsx
@@ -2,9 +2,9 @@ export function FaqBanner() {
   return (
     <div>
       <div className="bg-center bg-right sm:bg-right md:bg-right md:bg-center relative overflow-hidden pb-5 bg-[#121111] border-t-2 border-[#3e3b3c]">
-        <div className="h-[250px] flex flex-col items-center justify-center h-full">
-          <div className="text-white">
-            <span className="md:text-[100px] text-[50px] sm:text-[50px] font-extrabold md:leading-[80px] mb-[-0.5rem]">
+        <div className="h-[250px] flex flex-col items-center justify-center">
+          <div className="text-white" style={{fontFamily: 'Bungee'}} >
+            <span className="md:text-[100px] text-[50px] sm:text-[50px] md:leading-[80px] mb-[-0.5rem]">
               FAQS
             </span>
           </div>
