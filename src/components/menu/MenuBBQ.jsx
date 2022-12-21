6efb938333af5f88f6f5a152604ee6e9b281944e
smartcleancode <smartdevcode@gmail.com>
2022-12-21 01:22:15 +0100

    Finished web design of menu page

--- src/components/menu/MenuBBQ.jsx
+++ src/components/menu/MenuBBQ.jsx
@@ -1,13 +1,13 @@
 export function MenuBbq() {
   return (
-    <div className="bg-center relative flex items-center min-h-[70vh] bg-[#231F20]">
-      <div className="flex flex-wrap items-center h-full">
-        <div className="flex-1">
-          <img className="absolute top-[-15%] left-[-9%] w-[45%] z-[1]" src={'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto/fb/web/menu/bbq_feast.png'} alt='' />
+    <div className="bg-center relative min-h-[85vh] bg-[#231F20]">
+      <div className="grid  grid-cols-2 flex-wrap h-full">
+        <div className="flex-1 relative">
+          <img className="absolute top-[-20%] left-[-12%] w-[100%] z-[1]" src={'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto/fb/web/menu/bbq_feast.png'} alt='' />
         </div>
         <img className="absolute right-0 top-[-30%]" src={'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto/fb/web/about_us/arrows_down.png'} alt='' />
 
-        <div className="flex-1 flex flex-col lg:pr-[15%] lg:mb-[50px] relative">
+        <div className="flex-1 flex flex-col lg:pr-[15%] py-[15%] lg:mb-[50px] relative">
           <h3 className="lg:text-[5rem] md:text-[4rem] sm-max:text-[3rem] text-white" style={{fontFamily:'Bungee'}}>BBQ FEAST</h3>
           <p className="lg:text-[30px] md:text-[25px] sm-max:text-[20px] tracking-tighter text-white">WARNING: may include fall-off-the-bone tender brisket, mac n’ cheese that puts your mom’s recipe to shame, the fluffiest rolls you’ve ever sunk your teeth into, and a mid-life crisis where you quit your corporate job, move to a farm in Kansas, and adopt a horse named Sally.</p>
         </div>
--- src/components/menu/MenuMexican.jsx
+++ src/components/menu/MenuMexican.jsx
@@ -1,11 +1,13 @@
 export function MenuMexican() {
   return (
-    <div className="bg-center relative flex items-center min-h-[70vh] bg-[#231F20]">
-      <div className="flex flex-wrap items-center h-full">
-        <div className="flex-1">
-          <img className="absolute top-[-15%] left-[-9%] w-[45%] z-[1]" src={'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto/fb/web/menu/mexican_feast.png'} alt='' />
+    <div className="bg-center relative flex min-h-[85vh] bg-[#231F20]">
+      <div className="grid  grid-cols-2 flex-wrap h-full">
+        <div className="flex-1 relative">
+          <img className="absolute top-[-15%] left-[-9%] w-[100%] z-[1]" src={'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto/fb/web/menu/mexican_feast.png'} alt='' />
         </div>
-        <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col relative pr-[15%] pt-[35%]">
+          <img className="absolute right-0 top-[-20%]" src={'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto/fb/web/menu/triangles_1.png'} alt='' />
+          <img className="absolute m-auto left-[-30%] bottom-[-25%] z-[3] w-[20%]" src={'https://res.cloudinary.com/meals/image/upload/f_auto,q_auto/fb/web/about_us/arrows_down.png'} alt='' />
           <h3 className="lg:text-[5rem] md:text-[4rem] sm-max:text-[3rem] text-white" style={{fontFamily:'Bungee'}}>MEXICAN FEAST</h3>
           <p className="lg:text-[30px] md:text-[25px] sm-max:text-[20px] tracking-tighter text-white">Have you ever suffered from uncontrollable drooling, overwhelming endorphin release, better digestion and a proud doctor (gotta love that bean fiber), and a sudden yet oddly strong urge to get into the tortilla making business?! No? Well you will now. (Some advice? Leave the tortillas to the experts, like us.)</p>
         </div>
