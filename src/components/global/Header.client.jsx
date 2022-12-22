27a36fad45c8b373842879d09a17381f1aea388b
Md Abul Kalam <tipu4142@gmail.com>
2022-12-22 22:50:56 +0600

    fix cart icon

--- src/components/global/Header.client.jsx
+++ src/components/global/Header.client.jsx
@@ -309,9 +309,9 @@ function CartBadge({dark}) {
     <div
       className={`${
         dark
-          ? 'text-primary bg-contrast text-contrast bg-[#121111]'
-          : 'text-contrast bg-[#121111]'
-      } absolute bottom-1 right-1 text-[0.625rem] font-medium subpixel-antialiased h-3 min-w-[0.75rem] flex items-center justify-center leading-none text-center rounded-full w-auto px-[0.125rem] pb-px`}
+          ? 'text-black text-contrast bg-[#ffffff]'
+          : 'text-black text-contrast bg-[#ffffff]'
+      } absolute bottom-1 right-0 text-[0.625rem] font-bold subpixel-antialiased h-5 min-w-[1.25rem] flex items-center justify-center leading-none text-center rounded-full w-auto pb-px`}
     >
       <span>{totalQuantity}</span>
     </div>
