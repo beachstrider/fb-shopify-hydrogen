33c2dd92a4fce33d4d9a7c9fe21bb3973d304e17
Habib <habiburrahman0540@gmail.com>
2022-12-27 17:53:57 +0600

    Modal and Tabs design updated

--- src/components/Tabs.client.jsx
+++ src/components/Tabs.client.jsx
@@ -4,15 +4,15 @@ const Tabs = ({metafields}) => {
   const handleSelect=(index)=>{
     setSelectedTag(index)
   }
-
+console.log('metafields',metafields)
   return (
     <>
-    <div className="max-w-2xl mx-auto px-8 sm:px-0">
+    <div className="max-w-2xl mx-auto sm:px-0">
       <div className="sm:w-12/12 sm:mx-auto">
         <div 
           role="tablist"
           aria-label="tabs"
-          className="relative w-max mx-auto  grid grid-cols-2 items-center font-bold overflow-hidden"
+          className="relative w-max   grid grid-cols-2 items-center font-bold overflow-hidden"
         >
           <div className="absolute indicator my-auto top-0 bottom-0 left-0 shadow-md"></div>
           <button
@@ -21,7 +21,7 @@ const Tabs = ({metafields}) => {
             aria-controls="panel-1"
             id="tab-1"
             tabIndex="0"
-            className={selectedTag === 1 ?"relative block h-10 px-2 tab  text-[#DB9707]":"relative block h-10 px-2 tab"}
+            className={selectedTag === 1 ?"relative block h-10 text-left tab  text-[#DB9707]":"relative block h-10 px-2 tab"}
             onClick={()=>handleSelect(1)}
           >
             <span className={selectedTag===1?"text-[#DB9707] ":"text-black text-tab-underline"}>Details</span>
@@ -39,31 +39,31 @@ const Tabs = ({metafields}) => {
           </button>
        
         </div>
-        <div className="my-6 relative text-black">
+        <div className="my-2 relative text-black">
           <div
             role="tabpanel"
             id="panel-1"
             className={selectedTag ===1 ? "tab-panel text-center":"absolute top-0 invisible opacity-0 text-left"}
           >
                  <div className="text-left text-black">
-                  <p>{ metafields.find(x => x.key === "description").value }</p>
+                  <p>{ metafields?.find(x => x.key === "description").value }</p>
               <h3 className="font-bold text-lg">What's in the box</h3>
               <div>
                 <p className="font-bold text-sm">Main courses</p>
-                <span className="text-sm">{ metafields.find(x => x.key === "main_course").value }</span>
+                <span className="text-sm">{ metafields?.find(x => x.key === "main_course").value }</span>
               </div>
               <div className="py-2">
                 <p className="font-bold text-sm">Side Dishes</p>
-                <span className="text-sm">{ metafields.find(x => x.key === "side_dishes").value }</span>
+                <span className="text-sm">{ metafields?.find(x => x.key === "side_dishes").value }</span>
               </div>
               <div>
                 <p className="font-bold text-sm">Sauce</p>
-                <span className="text-sm">{ metafields.find(x => x.key === "sauce").value }</span>
+                <span className="text-sm">{ metafields?.find(x => x.key === "sauce").value }</span>
               </div>
             </div>
             <div className="text-left text-black">
               <h3 className="font-bold text-lg">Allergens</h3>
-              <span className="text-sm">{ metafields.find(x => x.key === "allergens").value }</span>
+              <span className="text-sm">{ metafields?.find(x => x.key === "allergens").value }</span>
             </div>
           </div>
           <div
--- src/components/shopping/MealItem.client.jsx
+++ src/components/shopping/MealItem.client.jsx
@@ -25,8 +25,8 @@ export function MealItem({title, image, metafields}) {
         <div className="text-center text-sm mb-2 ">Serves: 5</div>
       </button>
 
-      <MealModal open={openModal} close={() => setOpenModal(false)}>
-        <div className="w-full px-5 py-5">
+      {/* <MealModal open={openModal} close={() => setOpenModal(false)}>
+        <div className="w-[700px] px-5 py-5">
           <div>
             <img
               alt="Laptop"
@@ -45,7 +45,63 @@ export function MealItem({title, image, metafields}) {
             <Tabs metafields={metafields} />
           </div>
         </div>
-      </MealModal>
+      </MealModal> */}
+      {openModal ? (
+   <>
+   <div
+     className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+   >
+     <div className="relative w-auto my-10 mx-auto max-w-3xl">
+       {/*content*/}
+       <div className="border-0 shadow-lg relative flex flex-col w-80 sm:w-full bg-white outline-none focus:outline-none">
+         {/*header*/}
+         <button
+             type="button"
+             className="absolute -top-1 -right-1 rounded-full border border-gray-200 bg-white p-1 text-gray-800"
+              onClick={() => setOpenModal(false)}
+           >
+            <span className="sr-only">Close</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4"
+              viewBox="0 0 20 20"
+              fill="black 800"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+         </button>
+         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 px-5 py-5" >
+          <div>
+      <img
+        alt="Laptop"
+        src={
+          image
+            ? image
+            : 'https://www.freeiconspng.com/uploads/no-image-icon-6.png'
+        }
+        className="h-full w-full object-cover"
+      />
+        </div>
+      <div className='pl-5'>
+        <h1 className="text-lg font-bold text-left mb-4 text-black">
+          {title}
+        </h1>
+        <Tabs metafields={metafields} />
+        
+      </div>
+    </div>
+       
+      
+       </div>
+     </div>
+   </div>
+   <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+ </>
+      ):" "}
     </>
   );
 }
