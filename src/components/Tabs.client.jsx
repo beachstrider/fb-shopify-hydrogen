94b0ec89998fd044b5c7a916cddc4239d7ba9414
blackhorse0215 <blackhorse0215@gmail.com>
2022-12-27 22:13:58 -0600

    fix: feastbox product modal size and close button

--- src/components/Tabs.client.jsx
+++ src/components/Tabs.client.jsx
@@ -129,6 +129,7 @@ const Tabs = ({metafields}) => {
                 style={{
                   width: '100%',
                   height: '100%',
+                  minHeight: '348px',
                 }}
               />
             </div>
