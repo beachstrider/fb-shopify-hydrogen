05a23d8ef3c2c39a2a19fff4b6f4f76e68fb1be4
Md Abul Kalam <tipu4142@gmail.com>
2022-12-30 07:04:30 -0600

    remove unnecessary codes

--- src/routes/menus/index.server.jsx
+++ src/routes/menus/index.server.jsx
@@ -19,14 +19,6 @@ const Index = () => {
       <MenuFusion />
       <MenuChef />
       <div className='p-[50px] bg-[#DB9707] w-full'></div>
-      {/* <Section>
-        <div
-          id="version_mark"
-          className="fixed flex justify-center items-center right-40 top-0 mt-20 z-10 p-20 text-2xl bg-white bg-opacity-60"
-        >
-          BETA, Dec 20 - Web Developer
-        </div>
-      </Section> */}
     </Layout>
   );
 };
