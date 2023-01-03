3d59144c6d6f6461f1ee4fe5138f2f6b40f48cb0
Md Abul Kalam <tipu4142@gmail.com>
2023-01-03 16:44:03 -0600

    discount feature updated for subscription

--- src/utils/cost.js
+++ src/utils/cost.js
@@ -1,7 +1,6 @@
 import getSymbolFromCurrency from 'currency-symbol-map';
 
 export function getFullCost(cost, code) {
-  console.log('cost', cost);
   if (typeof cost === 'undefined') {
     return '';
   } else {
