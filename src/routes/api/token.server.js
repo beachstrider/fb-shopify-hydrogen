1aa7b2a5774349e63574fe638b574b93c4691e2b
Jason Heo <mernstackdev729@gmail.com>
2023-01-04 09:56:57 +0900

    add: env

--- src/routes/api/token.server.js
+++ src/routes/api/token.server.js
@@ -1,45 +1,45 @@
-const initialToken =
-  'ENkHRqH8tc9WuUAMV3N7zFYp38kayEWu4Gs2sTvwEe8M9h8CLDy3Ak3fB3Zsgv8WxDUf6ZHq9ufR42XBcE8JE9wUG83WfWz8ZQ68AaLxmnmWkD5LwrLeXBN2VPhVP6HhqV6zArVFAAAxmZwPBSuT7wSgvruspkd6xwhwfc9mLQ3NR7kdWyXJQsYjbgKZvNeD2Lt8J3P9e4aJuDtLXtbrmy964AX2uuyvqESzaNEDJPmJgsDLCaZv4LG6WnqcjZEh';
-
-const initialHeaders = {
-  Accept: 'application/json',
-  authorization: `Bearer ${initialToken}`,
-  'Content-Type': 'application/json',
-};
-
-// const baseURL = 'https://bundle-builder-api.speedwayapp.com/api/';
-const baseURL = 'https://bundle-builder-api-dev.speedwayapp.com/api/';
-
-// const shop = 'feastboxmeals.myshopify.com';
-const shop = 'feast-box-sandbox.myshopify.com';
-
-const bundleBuilder = async (
-  url,
-  params,
-  method = 'GET',
-  headers = {...initialHeaders},
-) => {
-  const res = await fetch(`${baseURL}${url}`, {
-    headers,
-    method,
-    ...(method !== 'GET' &&
-    typeof params === 'object' &&
-    Object.keys(params).length > 0
-      ? {body: JSON.stringify(params)}
-      : {}),
-  });
-
-  //because some api end point return 201, 204 eg. 201 is success response which create data in db
-  if (res.status >= 205) {
-    throw new Error('!!!');
-  }
-
-  const data = await res.json();
-
-  return data;
-};
-
 export async function api(request, {session}) {
+  // eslint-disable-next-line no-undef
+  const initialToken = Oxygen.env.BUNDLE_API_SECRET;
+
+  const initialHeaders = {
+    Accept: 'application/json',
+    authorization: `Bearer ${initialToken}`,
+    'Content-Type': 'application/json',
+  };
+
+  // eslint-disable-next-line no-undef
+  const baseURL = `https://${Oxygen.env.BUNDLE_API_DOMAIN}/api/`;
+
+  // eslint-disable-next-line no-undef
+  const shop = Oxygen.env.STORE_DOMAIN;
+
+  const bundleBuilder = async (
+    url,
+    params,
+    method = 'GET',
+    headers = {...initialHeaders},
+  ) => {
+    const res = await fetch(`${baseURL}${url}`, {
+      headers,
+      method,
+      ...(method !== 'GET' &&
+      typeof params === 'object' &&
+      Object.keys(params).length > 0
+        ? {body: JSON.stringify(params)}
+        : {}),
+    });
+
+    //because some api end point return 201, 204 eg. 201 is success response which create data in db
+    if (res.status >= 205) {
+      throw new Error('!!!');
+    }
+
+    const data = await res.json();
+
+    return data;
+  };
+
   if (session) {
     //this token preserve for long time but in our api end expire that token which cause api error
     try {
