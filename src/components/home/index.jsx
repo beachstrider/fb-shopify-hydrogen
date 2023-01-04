13c0953a5fa734a8c87ba43a087f783463540307
Jason Heo <mernstackdev729@gmail.com>
2023-01-05 05:31:39 +0900

    add: addGetEating

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,5 +1,5 @@
 export {HeroInnerSection} from './HeroInnerSection.client';
-export {HeroSection} from './HeroSection';
+export {HeroSection} from './HeroSection.client';
 export {PartyTime} from './PartyTime';
 export {CounterSection} from './CounterSection.client';
 export {FanClub} from './FanClub';
