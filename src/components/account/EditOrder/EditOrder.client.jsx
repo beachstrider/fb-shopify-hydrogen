c47f7ca47375a8fc8abde6ea64bda96a98143d05
smartcleancode <smartdevcode@gmail.com>
2023-01-07 08:14:32 +0100

    added products list in edit order page

--- src/components/account/EditOrder/EditOrder.client.jsx
+++ src/components/account/EditOrder/EditOrder.client.jsx
@@ -1,4 +1,49 @@
-export function EditOrder() {
+import {useState, useEffect, useRef} from 'react';
+import axios from 'axios';
+
+export function EditOrder({subid,date}) {
+
+  const [productlist, setProductlist] = useState([])
+  
+
+  useEffect(()=>{
+    const subOrders = async ()=>{
+      var subData = await axios.get(
+        `/api/bundleAuth/subscriptions/${subid}/orders`,
+      );
+      var bundleItem = await axios.get(
+        `/api/bundleAuth/bundles/${subData.data[0].subscription.bundle_id}`
+      )
+      const content = await axios.get(
+        `/api/bundleAuth/bundles/${subData.data[0].subscription.bundle_id}/configurations/${bundleItem.data.configurations[0].id}/contents?is_enabled=1&deliver_after=${date}`
+      );
+      const product_ids = [];
+      content.data[0].products.map((pro)=>{
+        product_ids.push(pro.platform_product_id)
+      })
+
+      const {data: products} = await axios.post(
+        `/api/products/multiple`,
+        {
+          product_ids,
+        },
+      );
+      
+      
+      for(var i = 0; i < products.length; i ++){
+        for(var j = 0; j < products[i].variants.nodes.length; j++){
+          products[i].variants.nodes[j].id = products[i].variants.nodes[j].id.replace("gid://shopify/ProductVariant/","")
+        }
+      }
+      setProductlist(products)
+      console.log(products)
+      // console.log(products)
+      // console.log("SubData", subData)
+      // console.log("BundleItem", bundleItem)
+      // console.log("CONTENT", content)
+    } 
+    subOrders()
+  },[])
   return (
     <section className="">
       <div className="">
@@ -8,9 +53,9 @@ export function EditOrder() {
           </h1>
           <div className="text-xl  font-medium mt-[19px] lg:ml-[451px]">
             0 Meals Left{' '}
-            <button className="bg-[#DB9707] px-3 py-1 rounded-sm text-white">
+            {/* <button className="bg-[#DB9707] px-3 py-1 rounded-sm text-white">
               Clear Selections
-            </button>
+            </button> */}
           </div>
         </div>
       </div>
@@ -20,41 +65,47 @@ export function EditOrder() {
 
         <div className="flex flex-wrap -mx-2 -mb-2">
           {/*--1----*/}
-          <div className="w-1/3 lg:w-1/6 sm:w-1/3 md:w-1/3 p-2 border">
-            <div className="text-center">
-              <button
-                className=" text-center font-bold font-heading"
-                href="#"
-              >
-                <img
-                  className="mx-auto object-contain"
-                  src="https://res.cloudinary.com/meals/image/upload/f_auto,q_auto,w_150/fb/web/shop/fb_meal_placeholder.png"
-                  alt="img"
-                />
-                <h3 className="font-bold font-heading text-sm text-center">
-                  BBQ FEASTbox
-                </h3>
-                <div className="text-center text-sm mb-2 ">Serves: 5</div>
-              </button>
-              <div className="px-4 mb-4 xl:mb-0 text-center">
-                <button
-                  className=" text-center text-white font-bold font-heading uppercase transition "
-                  href="#"
-                  style={{
-                    backgroundColor: '#DB9707',
-                    color: '#FFFFFF',
-                    width: 80,
-                    padding: '3px 21px',
-                  }}
-                >
-                  Add+
-                </button>
+          {productlist.map((item, key)=>(
+            item.variants.nodes.map((variant,v_index)=>(
+              <div className="w-1/3 lg:w-1/6 sm:w-1/3 md:w-1/3 p-2 border m-[5px]" key={v_index}>
+                <div className="text-center">
+                  <button
+                    className=" text-center font-bold font-heading"
+                    href="#"
+                  >
+                    <img
+                      className="mx-auto object-contain"
+                      src={variant.image.url}
+                      alt="img"
+                    />
+                    <h3 className="font-bold font-heading text-sm text-center">
+                      BBQ FEASTbox
+                    </h3>
+                    <div className="text-center text-sm mb-2 ">Serves: 5</div>
+                  </button>
+                  <div className="px-4 mb-4 xl:mb-0 text-center">
+                    <button
+                      className=" text-center text-white font-bold font-heading uppercase transition "
+                      href="#"
+                      style={{
+                        backgroundColor: '#DB9707',
+                        color: '#FFFFFF',
+                        width: 80,
+                        padding: '3px 21px',
+                      }}
+                    >
+                      Add+
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            ))
+            
+          ))}
+          
         </div>
       </div>
-      <div className="fixed bottom-[0px] bg-white py-6 px-12 w-[100%] md:w-[100%] lg:w-[68.5%] shadow-2xl flex justify-between">
+      <div className="m-auto mt-[100px] bg-white py-6 px-12 w-[100%] md:w-[100%] lg:w-[68.5%] flex justify-between">
         <button className="border-2 border-red-500 px-7 py-4 rounded-full hover:bg-red-500 font-semibold text-xl hover:text-white">
           Cancel
         </button>
--- src/routes/account/subscriptions/[id]/edit-order/[subid].server.jsx
+++ src/routes/account/subscriptions/[id]/edit-order/[subid].server.jsx
@@ -4,24 +4,32 @@ import {
   Seo,
   useSession,
   useLocalization,
+  useRouteParams,
   useShopQuery,
   useServerAnalytics,
+  useUrl
 } from '@shopify/hydrogen';
 import '../../../../../assets/CSS/style.css';
 import {Layout} from '~/components/index.server';
 import {AccountPageLayout} from '~/components/account/AccountPageLayout.client';
 import {EditOrder} from '~/components';
+import {
+  encryptSubId,
+  decryptSubId,
+} from '~/utils/common';
 import {CUSTOMER_QUERY} from '~/lib/queries';
 
 const Index = ({response}) => {
   response.cache(CacheNone());
-
+  const {subid} = useRouteParams();
+  const de_subid = decryptSubId(subid);
+  const date = useUrl().searchParams.get("date");
+  console.log(date);
   const {
     language: {isoCode: languageCode},
     country: {isoCode: countryCode},
   } = useLocalization();
   const {customerAccessToken} = useSession();
-
   if (!customerAccessToken) return response.redirect('/account/login');
 
   const {data} = useShopQuery({
@@ -51,7 +59,7 @@ const Index = ({response}) => {
         <Seo type="noindex" data={{title: 'FeastBox How it works page'}} />
       </Suspense>
       <AccountPageLayout user={customer} currentPath="subscriptions">
-        <EditOrder />
+        <EditOrder subid={de_subid} date={date} />
       </AccountPageLayout>
     </Layout>
   );
